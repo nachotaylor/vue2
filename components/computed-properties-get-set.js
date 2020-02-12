f11fb251627573bb869673048c94a832b815920c
Nacho <nacho_taylor@hotmail.com>
2020-02-12 17:20:39 -0300

    Computed properties getter&setter

---
+++ components/computed-properties-get-set.js
@@ -0,0 +1,24 @@
+Vue.component('computed-properties-get-set', {
+    data() {
+        return {
+            amount: 0
+        }
+    },
+    computed: {
+        amuountFormatted: {
+            get() {
+                return `${this.amount}$`;
+            },
+            set(newValue) {
+                $this.amount = newValue;
+            }
+        }
+    },
+    template: `
+        <div>
+            <h2>Computed properties</h2>
+            <input v-model="amount">
+            <p>{{ amuountFormatted }}</p>
+        </div>
+    `
+});
