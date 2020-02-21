5436a5b33cf74fa36561f5925386f97b2649eb43
Nacho <nacho_taylor@hotmail.com>
2020-02-21 14:53:13 -0300

    List products

--- vue-shop/src/components/ProductList.vue
+++ vue-shop/src/components/ProductList.vue
@@ -1,19 +1,27 @@
 <template>
     <div v-if="products.length">
         <paginate name="products" :list="products" :per="perPage">
-            <p v-for="product in paginated('products')">{{ product.name }}</p>
+            <b-card-group columns>
+                <product-item v-for="product in paginated('products')" :product="product" :key="product.id"
+                              @addToCart="AddProductToCart">
+                </product-item>
+            </b-card-group>
         </paginate>
         <paginate-links for="products" :classes="{ 'ul': 'pagination', 'li': 'page-item', 'li > a': 'page-link' }">
-            
+
         </paginate-links>
     </div>
-    <b-alert v-else></b-alert>
+    <b-alert v-else show variant="info">No hay productos</b-alert>
 </template>
 
 <script>
     import {mapActions, mapMutations, mapState} from 'vuex'
+    import ProductItem from "./ProductItem";
 
     export default {
+        components: {
+            ProductItem
+        },
         mounted() {
             this.fetchProducts()
         },
@@ -27,7 +35,9 @@
             ...mapState('products', ['products'])
         },
         methods: {
-            ...mapActions('products', ['fetchProducts'])
+            ...mapActions('products', ['fetchProducts']),
+            AddProductToCart(product) {
+            }
         }
     }
 </script>
