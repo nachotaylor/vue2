682913172b81e093405917819c97ff17883936ec
Nacho <nacho_taylor@hotmail.com>
2020-02-26 09:47:20 -0300

    Create cart

--- vue-shop/src/store.js
+++ vue-shop/src/store.js
@@ -3,10 +3,12 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 import products from './modules/products'
+import cart from './modules/cart'
 
 export default new Vuex.Store({
     state: {},
     modules: {
-        products
+        products,
+        cart
     }
 })
