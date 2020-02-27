47f51fb3f1af2fbd7e75409b37d333b78ee6a75b
Nacho <nacho_taylor@hotmail.com>
2020-02-27 10:30:22 -0300

    Add & remove items to cart

--- vue-shop/src/App.vue
+++ vue-shop/src/App.vue
@@ -1,11 +1,11 @@
 <template>
-    <div id="app">
-        <shop-layaout class="mt-5">
+    <div id="app" class="mt-5">
+        <shop-layaout>
             <template slot="product-list">
                 <product-list></product-list>
             </template>
             <template slot="cart">
-                Carrito
+                <cart></cart>
             </template>
         </shop-layaout>
     </div>
@@ -14,12 +14,14 @@
 <script>
     import ProductList from "@/components/ProductList";
     import ShopLayaout from "@/components/ShopLayaout";
+    import Cart from "@/components/Cart";
 
     export default {
         name: 'app',
         components: {
             ProductList,
-            ShopLayaout
+            ShopLayaout,
+            Cart
         }
     }
 </script>
