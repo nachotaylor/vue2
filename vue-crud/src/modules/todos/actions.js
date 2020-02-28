faf48a11e0afedf7adee4fcd569b32f090540f8f
Nacho <nacho_taylor@hotmail.com>
2020-02-28 12:14:10 -0300

    Define api actinos

--- vue-crud/src/modules/todos/actions.js
+++ vue-crud/src/modules/todos/actions.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 
-export async function fetchTdos({commit}) {
+export async function fetchTodos({commit}) {
     try {
         const {data} = await Vue.axios({
             url: '/todos'
@@ -9,6 +9,74 @@ export async function fetchTdos({commit}) {
     } catch (e) {
         commit('todosError', e.message)
     } finally {
-        console.log('Peticion finalizada')
+        console.log('Peticion GET finalizada')
+    }
+}
+
+export async function addTodo({commit}, todo) {
+    try {
+        await Vue.axios({
+            methods: 'POST',
+            url: '/todos',
+            data: {
+                id: Date.now(),
+                text: todo.text,
+                done: todo.done
+            }
+        })
+    } catch (e) {
+        commit('todosError', e.message)
+    } finally {
+        console.log('Peticion POST finalizada')
+    }
+}
+
+export async function updateTodo({commit}, todo) {
+    try {
+        await Vue.axios({
+            methods: 'PUT',
+            url: `/todos/${todo.id}`,
+            data: {
+                id: todo.id,
+                text: todo.text,
+                done: todo.done
+            }
+        })
+    } catch (e) {
+        commit('todosError', e.message)
+    } finally {
+        console.log('Peticion PUT finalizada')
+    }
+}
+
+export async function updateTodoStatus({commit}, todo) {
+    try {
+        await Vue.axios({
+            methods: 'PUT',
+            url: `/todos/${todo.id}`,
+            data: {
+                id: todo.id,
+                text: todo.text,
+                done: !todo.done
+            }
+        })
+    } catch (e) {
+        commit('todosError', e.message)
+    } finally {
+        console.log('Peticion PUT finalizada')
+    }
+}
+
+export async function deleteTodo({commit, dispatch}, todo) {
+    try {
+        await Vue.axios({
+            methods: 'DELETE',
+            url: `/todos/${todo.id}`
+        })
+        dispatch('fetchTodos')
+    } catch (e) {
+        commit('todosError', e.message)
+    } finally {
+        console.log('Peticion PUT finalizada')
     }
 }
