422387223fa7a4d04b8d4c469fa028d056f6ebdc
Nacho <nacho_taylor@hotmail.com>
2020-02-28 12:46:53 -0300

    Create component

---
+++ vue-crud/src/components/TodoList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-if="todos.length">
+
+    </div>
+    <b-alert show variant="info" v-else>No hay todos</b-alert>
+</template>
+
+<script>
+    export default {
+        props: {
+            todos: {
+                type: Array,
+                required: true
+            }
+        }
+    }
+</script>
