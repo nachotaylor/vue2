6ef2632e4a28d613d2a2d72d52d1bd107a57ab8a
Nacho <nacho_taylor@hotmail.com>
2020-03-02 17:50:53 -0300

    Add validations

---
+++ vue-crud/src/components/TodoForm.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    import {validationMixin} from 'vuelidate'
+    import {required, minLength} from 'vuelidate/lib/validators'
+
+    export default {
+        props: {
+            todo: {
+                type: Object,
+                required: true
+            },
+            todoSubmit: {
+                type: String,
+                default: 'Crear todo'
+            }
+        },
+        validation: {
+            todo: {
+                text: {
+                    required, minLength: minLength(4)
+                }
+            }
+        }
+    }
+</script>
