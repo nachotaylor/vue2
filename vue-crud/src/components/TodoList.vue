ee875ba6ccb0fb63fe1dcb98366b1cce03c6ffb2
Nacho <nacho_taylor@hotmail.com>
2020-03-02 12:28:31 -0300

    Add methods to components

--- vue-crud/src/components/TodoList.vue
+++ vue-crud/src/components/TodoList.vue
@@ -1,12 +1,17 @@
 <template>
     <div v-if="todos.length">
-
+        <todo-item v-for="todo in todos" :key="todo.id" :todo="todo"></todo-item>
     </div>
     <b-alert show variant="info" v-else>No hay todos</b-alert>
 </template>
 
 <script>
+    import TodoItem from "./TodoItem";
+
     export default {
+        components: {
+            TodoItem
+        },
         props: {
             todos: {
                 type: Array,
